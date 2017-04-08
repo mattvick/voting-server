a85c8c690402db74b964efaff530ffc25e560379
Matthew <matthew@Air.local>
2017-04-08 14:21:05 +0300

    Remove commented code

--- test/core_spec.js
+++ test/core_spec.js
@@ -46,18 +46,6 @@ describe('application logic', () => {
         entries: List()
       }));
     });
-    // it('creates a tally for the voted entry', () => {
-    //   const state = Map({
-    //     pair: List.of('Trainspotting', '28 Days Later')
-    //   });
-    //   const nextState = vote(state, 'Trainspotting')
-    //   expect(nextState).to.equal(Map({
-    //     pair: List.of('Trainspotting', '28 Days Later'),
-    //     tally: Map({
-    //       'Trainspotting': 1
-    //     })
-    //   }));
-    // });
 
     it('adds to existing tally for the voted entry', () => {
       const state = Map({
@@ -82,23 +70,6 @@ describe('application logic', () => {
         entries: List()
       }));
     });
-    // it('adds to existing tally for the voted entry', () => {
-    //   const state = Map({
-    //     pair: List.of('Trainspotting', '28 Days Later'),
-    //     tally: Map({
-    //       'Trainspotting': 3,
-    //       '28 Days Later': 2
-    //     })
-    //   });
-    //   const nextState = vote(state, 'Trainspotting');
-    //   expect(nextState).to.equal(Map({
-    //     pair: List.of('Trainspotting', '28 Days Later'),
-    //     tally: Map({
-    //       'Trainspotting': 4,
-    //       '28 Days Later': 2
-    //     })
-    //   }));
-    // });
 
   });
 
